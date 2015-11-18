8017918c5880f0d8a83b39ab3095f648ad113b70
Coralain <carolain.anto@gmail.com>
2015-11-18 13:50:48 -0500

    Servicios creados y entity model

---
+++ AirlineServices/ServicioHistorialPasajero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AirlineServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioHistorialPasajero" in both code and config file together.
+    public class ServicioHistorialPasajero : IServicioHistorialPasajero
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
