2adf2a0b59fc07512e4a000a38372c7a7ca3510b
jackqueline93 <jackqueline_93@hotmail.com>
2015-11-20 06:50:43 -0500

    Servicio.Visitas.V2
    Web Service WCF (IMPLEMENTACION) vISITAS.

--- AirlineServices/IServicioVisitas.cs
+++ AirlineServices/IServicioVisitas.cs
@@ -12,6 +12,96 @@ namespace AirlineServices
     public interface IServicioVisitas
     {
         [OperationContract]
-        void DoWork();
+        List<Visitas> GetVisitas_X_Fecha(DateTime fechaInicial, DateTime fechaFinal);
+        [OperationContract]
+        List<Visitas> GetVisitas_X_LugarOrigen(string LugarOrigen, DateTime fechaInicial, DateTime fechaFinal);
+        [OperationContract]
+        List<Visitas> AllVisitas();
+        [OperationContract]
+        List<Visitas> RankingVisitasDestinosPeruanos(DateTime fechaLLegada);
+    }
+
+    [DataContract]
+    [Serializable]
+    public class Visitas  // definimos la clase Vendedor como Data Contractual
+    {
+        //Miembros privados
+        private String _nombrePasajero;
+        [DataMember]
+        public String NombrePasajero
+        {
+            get { return _nombrePasajero; }
+            set { _nombrePasajero = value; }
+        }
+
+
+        private DateTime _ApellidoPasajero;
+        [DataMember]
+        public DateTime ApellidoPasajero
+        {
+            get { return _ApellidoPasajero; }
+            set { _ApellidoPasajero = value; }
+        }
+
+
+
+
+
+        private DateTime _fechaDespeque;
+        [DataMember]
+        public DateTime FechaDespeque
+        {
+            get { return _fechaDespeque; }
+            set { _fechaDespeque = value; }
+        }
+
+        private DateTime _fechaLlegada;
+        [DataMember]
+        public DateTime FechaLlegada
+        {
+            get { return _fechaLlegada; }
+            set { _fechaLlegada = value; }
+        }
+
+
+
+        private String _nomRuta;
+        [DataMember]
+        public String NomRuta
+        {
+            get { return _nomRuta; }
+            set { _nomRuta = value; }
+        }
+
+
+        private String _codigoDestino;
+        [DataMember]
+        public String CodigoDestino
+        {
+            get { return _codigoDestino; }
+            set { _codigoDestino = value; }
+        }
+
+
+
+
+        // Cada propiedad es un DataMember de la Data Contractual
+
+        private int _rankingVisitasDestinosPeruanos;
+        [DataMember]
+        public int RankingVisitasDestinosPeruanos
+        {
+            get { return _rankingVisitasDestinosPeruanos; }
+            set { _rankingVisitasDestinosPeruanos = value; }
+        }
+
+        private string _nombreLugar;
+        [DataMember]
+        public string NombreLugar
+        {
+            get { return _nombreLugar; }
+            set { _nombreLugar = value; }
+        }
+
     }
 }
--- AirlineServices/ServicioVisitas.cs
+++ AirlineServices/ServicioVisitas.cs
@@ -10,8 +10,169 @@ namespace AirlineServices
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioVisitas" in both code and config file together.
     public class ServicioVisitas : IServicioVisitas
     {
-        public void DoWork()
+        
+
+        public List<Visitas> GetVisitas_X_Fecha(DateTime fechaInicial, DateTime fechaFinal)
         {
+            AerolineaEntities misAerolineas = new AerolineaEntities();
+            List<Visitas> objListaVisitas = new List<Visitas>();
+
+
+            try
+            {
+                // Obtenemos con LINQ el registro a consultar
+                var query = (from objVisitas in misAerolineas.VW_VISITAS
+                             let fullName = objVisitas.NOMBRE_PASAJERO + "_" + objVisitas.APELLIDO_PASAJERO
+                             where objVisitas.FECHA_DESPEQUE >= fechaInicial &&
+                                    objVisitas.FECHA_LLEGADA <= fechaFinal
+
+
+                             select new { fullName, objVisitas.NOM_RUTA, objVisitas.FECHA_DESPEQUE, objVisitas.FECHA_LLEGADA }).Take(100);
+                //objPasajero.NOMBRE_PASAJERO + ' ' + objPasajero.APELLIDO_PASAJERO
+                // ,objRuta.NOM_RUTA ).FirstOrDefault();
+
+
+                foreach (var resultado in query)
+                {
+                    Visitas objVisitasBE = new Visitas();
+                    //Obtenemos los datos del vendedor
+                    objVisitasBE.NombrePasajero = resultado.fullName;
+                    objVisitasBE.NomRuta = resultado.NOM_RUTA;
+                    objVisitasBE.FechaDespeque = Convert.ToDateTime(resultado.FECHA_DESPEQUE);
+                    objVisitasBE.FechaLlegada = Convert.ToDateTime(resultado.FECHA_LLEGADA);
+
+                    objListaVisitas.Add(objVisitasBE);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return objListaVisitas;
         }
+
+        public List<Visitas> GetVisitas_X_LugarOrigen(string LugarOrigen, DateTime fechaInicial, DateTime fechaFinal)
+        {
+            AerolineaEntities misAerolineas = new AerolineaEntities();
+            List<Visitas> objListaVisitasDestinos = new List<Visitas>();
+
+
+            try
+            {
+                // Obtenemos con LINQ el registro a consultar
+                var query = (from objVisitas in misAerolineas.VW_VISITAS
+                             let fullName = objVisitas.NOMBRE_PASAJERO + "_" + objVisitas.APELLIDO_PASAJERO
+                             where objVisitas.NOM_RUTA.StartsWith(LugarOrigen) || objVisitas.NOM_RUTA.Contains(LugarOrigen) &&
+                                    objVisitas.TOMO_VUELO == true &&
+                                    objVisitas.ESTADO_VUELO == false &&
+                                    objVisitas.FECHA_DESPEQUE >= fechaInicial &&
+                                    objVisitas.FECHA_LLEGADA <= fechaFinal &&
+                                    objVisitas.COD_RUTA.StartsWith("RULLI")
+                             select new { fullName, objVisitas.NOM_RUTA, objVisitas.FECHA_DESPEQUE, objVisitas.FECHA_LLEGADA }).Take(100);
+                //objPasajero.NOMBRE_PASAJERO + ' ' + objPasajero.APELLIDO_PASAJERO
+                // ,objRuta.NOM_RUTA ).FirstOrDefault();
+
+
+                foreach (var resultado in query)
+                {
+                    Visitas objVisitasBE = new Visitas();
+                    //Obtenemos los datos del vendedor
+                    objVisitasBE.NombrePasajero = resultado.fullName;
+                    objVisitasBE.NomRuta = resultado.NOM_RUTA;
+                    objVisitasBE.FechaDespeque = Convert.ToDateTime(resultado.FECHA_DESPEQUE);
+                    objVisitasBE.FechaLlegada = Convert.ToDateTime(resultado.FECHA_LLEGADA);
+
+
+
+                    // objListaReservacion.Add(objReservacionBE);
+                    objListaVisitasDestinos.Add(objVisitasBE);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return objListaVisitasDestinos;
+        }
+
+        public List<Visitas> AllVisitas()
+        {
+            AerolineaEntities misAerolineas = new AerolineaEntities();
+            List<Visitas> objListaVisitas = new List<Visitas>();
+
+
+            try
+            {
+                // Obtenemos con LINQ el registro a consultar
+                var query = (from objVisitas in misAerolineas.VW_VISITAS
+                             let fullName = objVisitas.NOMBRE_PASAJERO + "_" + objVisitas.APELLIDO_PASAJERO
+
+
+                             select new { fullName, objVisitas.NOM_RUTA }).Take(100);
+
+
+                foreach (var resultado in query)
+                {
+                    Visitas objVisitasBE = new Visitas();
+                    objVisitasBE.NombrePasajero = resultado.fullName;
+                    objVisitasBE.NomRuta = resultado.NOM_RUTA;
+
+                    objListaVisitas.Add(objVisitasBE);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return objListaVisitas;
+        }
+
+        public List<Visitas> RankingVisitasDestinosPeruanos(DateTime fechaLLegada)
+        {
+            
+            AerolineaEntities misAerolineas = new AerolineaEntities();
+           
+            List<Visitas> RankingVisitasDestinosPeruanos = new List<Visitas>();
+
+            /*
+            try
+            {
+                
+                var query = misAerolineas.(fechaLLegada).Take(100);
+
+
+                foreach (var resultado in query)
+                {
+
+                    
+                    Visitas objVisitasBE = new Visitas();
+                    objVisitasBE.RankingVisitasDestinosPeruanos = Convert.ToInt32(resultado.RANKING);
+                    objVisitasBE.NombreLugar = resultado.NOMBRE_LUGAR;
+                    objVisitasBE.CodigoDestino = resultado.COD_DESTINO;
+
+                    RankingVisitasDestinosPeruanos.Add(objVisitasBE);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            */
+
+            return RankingVisitasDestinosPeruanos;
+        }
+
+            
+        
+
     }
 }
