f621470260eb729d41c00a6255cdcf6a4d9479d1
Coralain <carolain.anto@gmail.com>
2015-11-20 18:11:12 -0500

    servicio migrantes listo

--- AirlineServices/IServicioMigrantes.cs
+++ AirlineServices/IServicioMigrantes.cs
@@ -12,6 +12,230 @@ namespace AirlineServices
     public interface IServicioMigrantes
     {
         [OperationContract]
-        void DoWork();
+        List<PaisBE> ListaPaises();
+
+        [OperationContract]
+        PaisBE ConsultaMaximoMigrantesNacionalidad(string codPaisNacimiento);
+
+        [OperationContract]
+        List<MigranteBE> ListaMigrantesInternacionales(string codPaisNacimiento, System.DateTime fecIni, System.DateTime fecFin);
+        [OperationContract]
+        List<MigranteBE> ListaMigrantesNacionales(string codPaisNacimiento, System.DateTime fecIni, System.DateTime fecFin);
+
+        [OperationContract]
+        List<MigranteBE> ListaMigrantesPaisesFechas(string codPaisOrigen, string codPaisDestino, System.DateTime fecIni, System.DateTime fecFin);
+
+        [OperationContract]
+        List<PaisBE> PaisMaximoMigrantes(string codPais, System.DateTime fecIni, System.DateTime fecFin);
+    }
+
+    [DataContract]
+    [Serializable]
+    public class PaisBE
+    {
+        private string _codPais;
+        private string _nomPais;
+        private string _moneda;
+        private string _idioma;
+        private int _cantVuelos;
+
+        [DataMember]
+        public string CodPais
+        {
+            get { return _codPais; }
+            set { _codPais = value; }
+        }
+        [DataMember]
+        public string NomPais
+        {
+            get { return _nomPais; }
+            set { _nomPais = value; }
+        }
+        [DataMember]
+        public string Moneda
+        {
+            get { return _moneda; }
+            set { _moneda = value; }
+        }
+        [DataMember]
+        public string Idioma
+        {
+            get { return _idioma; }
+            set { _idioma = value; }
+        }
+        [DataMember]
+        public int CantVuelos
+        {
+            get { return _cantVuelos; }
+            set { _cantVuelos = value; }
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class LugarBE
+    {
+        private string _codLugar;
+        private string _nomLugar;
+        private string _latitud;
+        private string _longitud;
+        private string _nomAeropuerto;
+        private string _codPais;
+
+        [DataMember]
+        public string CodLugar
+        {
+            get { return _codLugar; }
+            set { _codLugar = value; }
+        }
+
+        [DataMember]
+        public string NomLugar
+        {
+            get { return _nomLugar; }
+            set { _nomLugar = value; }
+        }
+
+        [DataMember]
+        public string Latitud
+        {
+            get { return _latitud; }
+            set { _latitud = value; }
+        }
+
+        [DataMember]
+        public string Longitud
+        {
+            get { return _longitud; }
+            set { _longitud = value; }
+        }
+
+        [DataMember]
+        public string NomAeropuerto
+        {
+            get { return _nomAeropuerto; }
+            set { _nomAeropuerto = value; }
+        }
+
+        [DataMember]
+        public string CodPais
+        {
+            get { return _codPais; }
+            set { _codPais = value; }
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class MigranteBE
+    {
+        private string _codMigrante;
+        private string _dni;
+        private string _nomMigrante;
+        private string _apeMigrante;
+        private char _genero;
+        private string _numTelefono;
+        private string _email;
+        private System.DateTime _fecNac;
+        private string _codPaisNacion;
+        private string _paisDestino;
+        private string _lugarDestino;
+
+        [DataMember]
+        public string CodMigrante
+        {
+            get { return _codMigrante; }
+            set { _codMigrante = value; }
+        }
+        [DataMember]
+        public string Dni
+        {
+            get { return _dni; }
+            set { _dni = value; }
+        }
+        [DataMember]
+        public string NomMigrante
+        {
+            get { return _nomMigrante; }
+            set { _nomMigrante = value; }
+        }
+        [DataMember]
+        public string ApeMigrante
+        {
+            get { return _apeMigrante; }
+            set { _apeMigrante = value; }
+        }
+        [DataMember]
+        public char Genero
+        {
+            get { return _genero; }
+            set { _genero = value; }
+        }
+        [DataMember]
+        public string NumTelefono
+        {
+            get { return _numTelefono; }
+            set { _numTelefono = value; }
+        }
+        [DataMember]
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        [DataMember]
+        public System.DateTime FecNac
+        {
+            get { return _fecNac; }
+            set { _fecNac = value; }
+        }
+        [DataMember]
+        public string CodPaisNacion
+        {
+            get { return _codPaisNacion; }
+            set { _codPaisNacion = value; }
+        }
+        [DataMember]
+        public string PaisDestino
+        {
+            get { return _paisDestino; }
+            set { _paisDestino = value; }
+        }
+        [DataMember]
+        public string LugarDestino
+        {
+            get { return _lugarDestino; }
+            set { _lugarDestino = value; }
+        }
+    }
+
+    [DataContract]
+    [Serializable]
+    public class RutaBE
+    {
+        private string _origen;
+        private string _destino;
+        private int _cantVuelos;
+
+        [DataMember]
+        public string Origen
+        {
+            get { return _origen; }
+            set { _origen = value; }
+        }
+
+        [DataMember]
+        public string Destino
+        {
+            get { return _destino; }
+            set { _destino = value; }
+        }
+
+        [DataMember]
+        public int cantVuelos
+        {
+            get { return _cantVuelos; }
+            set { _cantVuelos = value; }
+        }
     }
 }
--- AirlineServices/ServicioMigrantes.cs
+++ AirlineServices/ServicioMigrantes.cs
@@ -10,8 +10,295 @@ namespace AirlineServices
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServicioMigrantes" in both code and config file together.
     public class ServicioMigrantes : IServicioMigrantes
     {
-        public void DoWork()
+        public List<PaisBE> ListaPaises()
         {
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            List<PaisBE> objListaPais = new List<PaisBE>();
+
+            try
+            {
+                var query = from objPais in MiAerolinea.PAIS
+                            orderby objPais.NOMBRE_PAIS
+                            select new { Codigo = objPais.COD_PAIS, Nombre = objPais.NOMBRE_PAIS };
+
+                foreach (var resultado in query)
+                {
+                    PaisBE objPaisBE = new PaisBE();
+
+                    objPaisBE.CodPais = resultado.Codigo;
+                    objPaisBE.NomPais = resultado.Nombre;
+
+                    objListaPais.Add(objPaisBE);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return objListaPais;
+        }
+
+        public PaisBE ConsultaMaximoMigrantesNacionalidad(string codPaisNacimiento)
+        {
+            //Pais al han migrado más las personas de cierto pais
+
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            PaisBE objPaisBE = new PaisBE();
+
+            try
+            {
+                var query = MiAerolinea.usp_ConsultaMaximoMigrantesNacionalidad(codPaisNacimiento).FirstOrDefault();
+
+                objPaisBE.CodPais = query.COD_PAIS_DESTINO;
+                objPaisBE.NomPais = query.NOMBRE_PAIS_DESTINO;
+                objPaisBE.Idioma = query.IDIOMA_PAIS_DESTINO;
+                objPaisBE.Moneda = query.MONEDA_PAIS_DESTINO;
+                objPaisBE.CantVuelos = Convert.ToInt32(query.CANTIDAD_VUELOS);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objPaisBE;
+        }
+
+
+
+        public List<MigranteBE> ListaMigrantesInternacionales(string codPaisNacimiento, System.DateTime fecIni, System.DateTime fecFin)
+        {
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            List<MigranteBE> objListaBE = new List<MigranteBE>();
+
+            //SI es internacional devuelve datos con paises
+            //SI es nacional devuelve datos con lugares
+
+            try
+            {
+                var query = (from tbPasajero in MiAerolinea.PASAJERO
+                             join tbReservacion in MiAerolinea.RESERVACION
+                                        on tbPasajero.COD_PASAJERO equals tbReservacion.COD_PASAJERO
+                             join tbVuelo in MiAerolinea.VUELO
+                                        on tbReservacion.COD_VUELO equals tbVuelo.COD_VUELO
+                             join tbRuta in MiAerolinea.RUTA
+                                        on tbVuelo.COD_RUTA equals tbRuta.COD_RUTA
+                             join tbLugar in MiAerolinea.LUGAR
+                                        on tbRuta.COD_DESTINO equals tbLugar.COD_LUGAR
+                             join tbPais in MiAerolinea.PAIS
+                                        on tbLugar.COD_PAIS equals tbPais.COD_PAIS
+                             where tbPasajero.COD_PAIS == codPaisNacimiento &&
+                                    tbRuta.TIPO_RUTA == "INTERNACIONAL" &&
+                             tbVuelo.FECHA_LLEGADA >= fecIni &&
+                             tbVuelo.FECHA_LLEGADA <= fecFin
+
+                             select new
+                             {
+                                 Codigo = tbPasajero.COD_PASAJERO,
+                                 Nombre = tbPasajero.NOMBRE_PASAJERO,
+                                 Apellido = tbPasajero.APELLIDO_PASAJERO,
+                                 DNI = tbPasajero.DNI,
+                                 Genero = tbPasajero.SEXO,
+                                 NumTel = tbPasajero.NUMERO_TELEFONICO,
+                                 Email = tbPasajero.EMAIL,
+                                 FecNac = tbPasajero.FECHA_NACIMIENTO,
+                                 PaisDestino = tbPais.NOMBRE_PAIS
+                             }).Take(100);
+
+                foreach (var resultado in query)
+                {
+                    MigranteBE objMigranteBE = new MigranteBE();
+                    objMigranteBE.CodMigrante = resultado.Codigo;
+                    objMigranteBE.NomMigrante = resultado.Nombre;
+                    objMigranteBE.ApeMigrante = resultado.Apellido;
+                    objMigranteBE.Dni = resultado.DNI;
+                    objMigranteBE.Genero = resultado.Genero.ElementAt(0);
+                    objMigranteBE.NumTelefono = resultado.NumTel;
+                    objMigranteBE.Email = resultado.Email;
+                    objMigranteBE.FecNac = Convert.ToDateTime(resultado.FecNac);
+                    objMigranteBE.PaisDestino = resultado.PaisDestino;
+
+                    objListaBE.Add(objMigranteBE);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objListaBE;
+        }
+
+        public List<MigranteBE> ListaMigrantesNacionales(string codPaisNacimiento, System.DateTime fecIni, System.DateTime fecFin)
+        {
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            List<MigranteBE> objListaBE = new List<MigranteBE>();
+
+            try
+            {
+                var query = (from tbPasajero in MiAerolinea.PASAJERO
+                             join tbReservacion in MiAerolinea.RESERVACION
+                                        on tbPasajero.COD_PASAJERO equals tbReservacion.COD_PASAJERO
+                             join tbVuelo in MiAerolinea.VUELO
+                                        on tbReservacion.COD_VUELO equals tbVuelo.COD_VUELO
+                             join tbRuta in MiAerolinea.RUTA
+                                        on tbVuelo.COD_RUTA equals tbRuta.COD_RUTA
+                             join tbLugar in MiAerolinea.LUGAR
+                                        on tbRuta.COD_DESTINO equals tbLugar.COD_LUGAR
+                             where tbPasajero.COD_PAIS == codPaisNacimiento &&
+                                   tbLugar.COD_PAIS == codPaisNacimiento &&
+                             tbVuelo.FECHA_LLEGADA >= fecIni &&
+                             tbVuelo.FECHA_LLEGADA <= fecFin
+
+                             select new
+                             {
+                                 Codigo = tbPasajero.COD_PASAJERO,
+                                 Nombre = tbPasajero.NOMBRE_PASAJERO,
+                                 Apellido = tbPasajero.APELLIDO_PASAJERO,
+                                 DNI = tbPasajero.DNI,
+                                 Genero = tbPasajero.SEXO,
+                                 NumTel = tbPasajero.NUMERO_TELEFONICO,
+                                 Email = tbPasajero.EMAIL,
+                                 FecNac = tbPasajero.FECHA_NACIMIENTO,
+                                 LugarDestino = tbLugar.NOMBRE_LUGAR
+                             }).Take(100);
+
+                foreach (var resultado in query)
+                {
+                    MigranteBE objMigranteBE = new MigranteBE();
+                    objMigranteBE.CodMigrante = resultado.Codigo;
+                    objMigranteBE.NomMigrante = resultado.Nombre;
+                    objMigranteBE.ApeMigrante = resultado.Apellido;
+                    objMigranteBE.Dni = resultado.DNI;
+                    objMigranteBE.Genero = resultado.Genero.ElementAt(0);
+                    objMigranteBE.NumTelefono = resultado.NumTel;
+                    objMigranteBE.Email = resultado.Email;
+                    objMigranteBE.FecNac = Convert.ToDateTime(resultado.FecNac);
+                    objMigranteBE.LugarDestino = resultado.LugarDestino;
+
+                    objListaBE.Add(objMigranteBE);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objListaBE;
+        }
+
+        public List<MigranteBE> ListaMigrantesPaisesFechas(string codPaisOrigen, string codPaisDestino, System.DateTime fecIni, System.DateTime fecFin)
+        {
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            List<MigranteBE> objListaBE = new List<MigranteBE>();
+
+            try
+            {
+                var codLOrigen = from tbLugar in MiAerolinea.LUGAR
+                                 where tbLugar.COD_PAIS == codPaisOrigen
+                                 select tbLugar.COD_LUGAR;
+
+                var codLDestino = from tbLugar in MiAerolinea.LUGAR
+                                  where tbLugar.COD_PAIS == codPaisDestino
+                                  select tbLugar.COD_LUGAR;
+
+                var query = (from tbPasajero in MiAerolinea.PASAJERO
+                             join tbReservacion in MiAerolinea.RESERVACION
+                                        on tbPasajero.COD_PASAJERO equals tbReservacion.COD_PASAJERO
+                             join tbVuelo in MiAerolinea.VUELO
+                                        on tbReservacion.COD_VUELO equals tbVuelo.COD_VUELO
+                             join tbRuta in MiAerolinea.RUTA
+                                        on tbVuelo.COD_RUTA equals tbRuta.COD_RUTA
+                             where tbVuelo.FECHA_LLEGADA >= fecIni &&
+                                   tbVuelo.FECHA_LLEGADA <= fecFin &&
+                                   codLOrigen.Contains(tbRuta.COD_ORIGEN) &&
+                                   codLDestino.Contains(tbRuta.COD_DESTINO)
+
+                             select new
+                             {
+                                 Codigo = tbPasajero.COD_PASAJERO,
+                                 Nombre = tbPasajero.NOMBRE_PASAJERO,
+                                 Apellido = tbPasajero.APELLIDO_PASAJERO,
+                                 DNI = tbPasajero.DNI,
+                                 Genero = tbPasajero.SEXO,
+                                 NumTel = tbPasajero.NUMERO_TELEFONICO,
+                                 Email = tbPasajero.EMAIL,
+                                 FecNac = tbPasajero.FECHA_NACIMIENTO
+                             }).Take(100);
+
+                foreach (var resultado in query)
+                {
+                    MigranteBE objMigranteBE = new MigranteBE();
+                    objMigranteBE.CodMigrante = resultado.Codigo;
+                    objMigranteBE.NomMigrante = resultado.Nombre;
+                    objMigranteBE.ApeMigrante = resultado.Apellido;
+                    objMigranteBE.Dni = resultado.DNI;
+                    objMigranteBE.Genero = resultado.Genero.ElementAt(0);
+                    objMigranteBE.NumTelefono = resultado.NumTel;
+                    objMigranteBE.Email = resultado.Email;
+                    objMigranteBE.FecNac = Convert.ToDateTime(resultado.FecNac);
+                    objListaBE.Add(objMigranteBE);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objListaBE;
+        }
+
+        public List<PaisBE> PaisMaximoMigrantes(string codPais, DateTime fecIni, DateTime fecFin)
+        {
+            AerolineaEntities MiAerolinea = new AerolineaEntities();
+            List<PaisBE> paises = new List<PaisBE>();
+
+            try
+            {
+                var codLOrigen = from tbLugar in MiAerolinea.LUGAR
+                                 where tbLugar.COD_PAIS == codPais
+                                 select tbLugar.COD_LUGAR;
+
+                var codLPDestino = from tbLugar in MiAerolinea.LUGAR
+                                   where !codLOrigen.Contains(tbLugar.COD_LUGAR)
+                                   select tbLugar.COD_LUGAR;
+
+                var query = ((from tbVuelo in MiAerolinea.VUELO
+                              join tbRuta in MiAerolinea.RUTA
+                              on tbVuelo.COD_RUTA equals tbRuta.COD_RUTA
+                              join tbLugar in MiAerolinea.LUGAR
+                              on tbRuta.COD_DESTINO equals tbLugar.COD_LUGAR
+                              join tbPais in MiAerolinea.PAIS
+                              on tbLugar.COD_PAIS equals tbPais.COD_PAIS
+                              where codLOrigen.Contains(tbRuta.COD_ORIGEN)
+                              && codLPDestino.Contains(tbRuta.COD_DESTINO)
+                              && tbVuelo.FECHA_LLEGADA > fecIni
+                              && tbVuelo.FECHA_LLEGADA < fecFin
+                              select tbPais
+                             ).GroupBy(x => x).Select(g => new
+                             {
+                                 CodPais = g.Key.COD_PAIS,
+                                 NomPais = g.Key.NOMBRE_PAIS,
+                                 Idioma = g.Key.IDIOMA,
+                                 Moneda = g.Key.MONEDA,
+                                 CantidadVuelos = g.Count()
+                             })).OrderByDescending(x => x.CantidadVuelos);
+
+
+                foreach (var item in query)
+                {
+                    PaisBE objBE = new PaisBE();
+                    objBE.CodPais = item.CodPais;
+                    objBE.NomPais = item.NomPais;
+                    objBE.Idioma = item.Idioma;
+                    objBE.Moneda = item.Moneda;
+                    objBE.CantVuelos = item.CantidadVuelos;
+
+                    paises.Add(objBE);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return paises;
         }
     }
 }
